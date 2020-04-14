84f45a61be7054686eaf7e0c57b2054b7971cef9
isaman sangbamrung <towlovelove@gmail.com>
2020-04-15 01:49:13 +0700

    add: logo login page

--- src/views/login.vue
+++ src/views/login.vue
@@ -5,20 +5,33 @@
       <v-img :src="image" aspect-ratio="1.7"></v-img>
       <h3>WELCOME</h3>
       <div class="card-login">
-        <h3>เข้าสู่ระบบ </h3>
-        <br/>
+        <br />
+        <h3>เข้าสู่ระบบ</h3>
         <v-text-field
-            label="Username"
-            filled
-            rounded
-            small
-          ></v-text-field>
-          <v-text-field
-            label="Password"
-            filled
-            rounded
-          ></v-text-field>
+          class="styled-input"
+          label="Username"
+          filled
+          rounded
+          small
+        ></v-text-field>
+        <v-text-field
+          class="styled-input"
+          label="Password"
+          filled
+          rounded
+        ></v-text-field>
       </div>
+      <div class="span-text">
+        <span>ยังไม่ลงทะเบียน </span><span class="link-font">ลงทะเบียนที่นี่</span>
+      </div>
+      <v-btn class="ma-2" tile large color="blue" icon>
+      <v-icon>mdi-facebook</v-icon>
+    </v-btn>
+    <v-btn class="ma-2" tile large  icon>
+      <v-avatar size="30">
+        <v-img :src="logoGoogle" aspect-ratio="1.7"></v-img>
+      </v-avatar>
+    </v-btn>
     </div>
   </div>
 </template>
@@ -30,25 +43,34 @@ export default {
   name: "login",
   data() {
     return {
-      image: require("../assets/KORAT.png")
+      image: require("../assets/KORAT.png"),
+      logoGoogle: require("../assets/google.png")
     };
-  }
+  },
 };
 </script>
 <style scoped>
 .card-login {
-  border: 2px solid #B2EBF2;
+  border: 2px solid #b2ebf2;
   border-radius: 50px;
-  background-color: #B2EBF2;
-  height: 35vh;
+  background-color: #b2ebf2;
+  height: 38vh;
   text-align: center;
-  margin-top: 2% ;
-  margin-right: 7% ;
-  margin-left: 7% ;
+  margin-top: 2%;
+  margin-right: 10%;
+  margin-left: 10%;
   padding: 1%;
-  padding-left:20px ;
+  padding-left: 20px;
   padding-right: 20px;
-
-  
+}
+.styled-input {
+  height: 70px;
+  margin-top: 3%;
+}
+.span-text {
+  margin-top: 5px;
+}
+.link-font{
+  color: blue;
 }
 </style>
